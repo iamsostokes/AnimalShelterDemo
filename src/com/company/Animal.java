b718bc015baa91faa5f9405b1dce6b28bd6793eb
Jamez Stokes <jamezstokes@MacBook-Pro-7.local>
2019-02-11 16:05:52 -0500

    Made all of the properties to create an animal and created all of the getters and setters.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,77 @@
 package com.company;
 
 public class Animal {
+
+    private int iDNumber;
+    private String name;
+    private String animalTyoe;
+    private String breed;
+    private String color;
+    private int age;
+    private String arrivalDate;
+    private String status;
+
+    public int getiDNumber() {
+        return iDNumber;
+    }
+
+    public void setiDNumber(int iDNumber) {
+        this.iDNumber = iDNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAnimalTyoe() {
+        return animalTyoe;
+    }
+
+    public void setAnimalTyoe(String animalTyoe) {
+        this.animalTyoe = animalTyoe;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(String arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
